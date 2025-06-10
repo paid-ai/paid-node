4a4d953da38f9a3e9e5f06fdaf24beaed967b7da
Georgi Dankov <gddankov@gmail.com>
2025-06-10 18:36:15 +0100

    Fix Batch usage client

--- src/wrapper/BatchUsage.ts
+++ src/wrapper/BatchUsage.ts
@@ -1,22 +1,35 @@
-import { Usage as UsageFernClient } from "../api/resources/usage/client/Client";
+import * as core from "../core/index.js";
+import { Usage as UsageClient } from "../api/resources/usage/client/Client";
+import * as Paid from "../api/types/Signal.js";
 
-export class Usage extends UsageFernClient { // extend the Fern generated client
+export class Usage extends UsageClient {
 
   private signals: any[] = [];
 
-  public flush(requestOptions?: any): Promise<unknown[]> {
+  public flush(
+      requestOptions?: UsageClient.RequestOptions,
+  ): core.HttpResponsePromise<unknown[]> {
     return this.recordBulk({ signals: this.signals }, requestOptions);
   }
 
-  public recordUsage(signal: any, requestOptions?: any): void {
+  public recordUsage(
+      signal: Paid.Signal = {},
+      requestOptions?: UsageClient.RequestOptions,
+  ): core.HttpResponsePromise<unknown[]> {
     if (!this.signals) {
       this.signals = [];
     }
+
     this.signals.push(signal);
+
     if (this.signals.length >= 100) {
-      this.recordBulk({ signals: this.signals }, requestOptions);
+      const response = this.recordBulk({ signals: this.signals }, requestOptions);
       this.signals = [];
+
+      return response;
     }
+
+    return core.HttpResponsePromise.fromResult({ data: [], rawResponse: {} as core.RawResponse });
   }
 
 }
