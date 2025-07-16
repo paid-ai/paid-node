24cbb019c0ffbd2ee19235ef67356a52876027b0
Ata Altyyev <ata@paid.ai>
2025-07-17 04:18:57 +0500

    add langchain callback provider

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { PaidEnvironment } from "./environments.js";
 export { PaidOpenAI } from "./tracing/wrappers/openAiWrapper.js";
 export { PaidAnthropic } from "./tracing/wrappers/anthropicWrapper.js";
 export { PaidMistral } from "./tracing/wrappers/mistralWrapper.js";
+export { PaidLangChainCallback } from "./tracing/wrappers/langchainCallback.js";
