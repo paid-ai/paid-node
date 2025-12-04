af227705e50c7ff8af3355bc4f138899b5c917fb
danor1 <dan@paid.ai>
2025-12-04 15:33:09 +0000

    reset

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('jest').Config} */
 export default {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleNameMapper: {
-      "(.+)\.js$": "$1",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "(.+)\.js$": "$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/mock-server/setup.ts"],
+  transformIgnorePatterns: [
+    'node_modules/(?!(until-async|msw|@mswjs|@bundled-es-modules|@open-draft|@bundled-es-modules/statuses|strict-event-emitter)/)',
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest', // Transform JS files with ts-jest
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
     },
-    setupFilesAfterEnv: ["<rootDir>/tests/mock-server/setup.ts"],
-    transformIgnorePatterns: [
-      'node_modules/(?!(until-async|msw|@mswjs|@bundled-es-modules|@open-draft|@bundled-es-modules/statuses|strict-event-emitter)/)',
-    ],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.jsx?$': 'ts-jest', // Transform JS files with ts-jest
-    },
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
-    },
-    extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  };
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+};
