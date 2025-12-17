77f5c700e29f36c1dfaff1c09daa666f2e7e908a
danor1 <dan@paid.ai>
2025-12-17 10:33:20 +0000

    fix: bump version and reset configs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('jest').Config} */
 export default {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleNameMapper: {
-        "(.+)\.js$": "$1",
-    },
-    setupFilesAfterEnv: ["<rootDir>/tests/mock-server/setup.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "(.+)\.js$": "$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/mock-server/setup.ts"],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(until-async|msw|@mswjs|@bundled-es-modules|@open-draft|strict-event-emitter|outvariant|headers-polyfill)/)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+    '^.+\\.(js|jsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 };
