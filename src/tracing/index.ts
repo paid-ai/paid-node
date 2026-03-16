d402b0794a9e906bd7e7e2f5a62d78b9121f1fb8
Ata Altyyev <ata@paid.ai>
2026-03-16 18:56:11 +0000

    export paidAutoInstrument from index file

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -1,6 +1,16 @@
 import { getPaidTracer, getPaidTracerProvider, initializeTracing, trace, createPaidSpanProcessors } from "./tracing.js";
 import { signal } from "./signal.js";
 import { AISDKSpanProcessor } from "./aiSdkSpanProcessor.js";
+import { paidAutoInstrument } from "./autoInstrumentation.js";
 
 export type { InitializeTracingOptions } from "./tracing.js";
-export { getPaidTracer, getPaidTracerProvider, initializeTracing, trace, signal, AISDKSpanProcessor, createPaidSpanProcessors };
+export {
+    getPaidTracer,
+    getPaidTracerProvider,
+    initializeTracing,
+    trace,
+    signal,
+    AISDKSpanProcessor,
+    createPaidSpanProcessors,
+    paidAutoInstrument,
+};
